b835d0b668b057dad31dcc9939e9c98da4fd44c9
anshu <anshug370@gmail.com>
2025-08-31 02:10:40 +0530

    fix:  remove endpoints file and centerup all the code in main.py

--- Clip_Service/main.py
+++ Clip_Service/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-import endpoints as endpoints
+from PIL import Image
+from fastapi import UploadFile, File
+from transformers import CLIPProcessor, CLIPModel
+import torch
 
 app = FastAPI()
 
@@ -13,8 +16,26 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(endpoints.router)
-
 @app.get('/')
 def main():
-    return {"message": "Image_Visulaizer MicroService"}
+    return {"message": "Image_Visualizer MicroService"}
+
+clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+clip_processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+
+@app.post("/api/match")
+async def match_image(file: UploadFile = File(...)):
+    if file:
+        image = Image.open(file.file).convert("RGB")
+    else:
+        raise ValueError("No image file or URL provided.")
+    
+    inputs = clip_processor(images=image, return_tensors="pt")
+
+    with torch.no_grad():
+        outputs = clip_model.get_image_features(**inputs)
+
+    embedding = outputs[0].numpy()
+
+    return {"message": "Matching the Image", "embedding": embedding.tolist()}

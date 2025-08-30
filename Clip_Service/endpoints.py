a565e28e3038142fe2b5424f6a559145128a827e
anshu <anshug370@gmail.com>
2025-08-31 01:33:16 +0530

    fix: out of memory error

--- Clip_Service/endpoints.py
+++ Clip_Service/endpoints.py
@@ -6,23 +6,23 @@ import torch
 
 router = APIRouter()
 
-def get_file(file: UploadFile = File(...)) -> Image.Image:
-    if file:
-        image = Image.open(file.file).convert("RGB")
-    else:
-        raise ValueError("No image file or URL provided.")
-    return image
-
 clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
 clip_processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
 
 @router.post("api/match")
-async def match_image(image: UploadFile = File(...)):
-    image = get_file(image)
+async def match_image(file: UploadFile = File(...)):
+    if file:
+        image = Image.open(file.file).convert("RGB")
+    else:
+        raise ValueError("No image file or URL provided.")
+    
     inputs = clip_processor(images=image, return_tensors="pt")
+
     with torch.no_grad():
         outputs = clip_model.get_image_features(**inputs)
+
     embedding = outputs[0].numpy()
+    
     return {"message": "Matching the Image", "embedding": embedding}
 
